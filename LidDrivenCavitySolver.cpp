9e637d66a8ec62eae551eb2130d0ed1040cb709a
Easha Easha Tu Razia <eh2617@spitfire.ae.ic.ac.uk>
2020-03-22 02:33:49 +0000

    command line input fixed

--- LidDrivenCavitySolver.cpp
+++ LidDrivenCavitySolver.cpp
@@ -1,16 +1,17 @@
 #include <mpi.h> // For MPI
 using namespace std;
 
-//#include "LidDrivenCavity.h"
-#include "LidDrivenCavityP.h"
+//#include "LidDrivenCavity.h" //Serial
+//#include "LidDrivenCavityP.h" //Scalapack
+#include "LidDrivenCavityJacobi.h" //Jacobi
 
 int main(int argc, char *argv[]){
     // Command line input
 //    int n = argc;
     int n=19; 
     //string n_input [n] = {argv};
-    string n_input [n] = {" ","--Lx", "1.0", "--Ly", "1.0", "--Nx", "12", "--Ny", 
-                            "12", "--Px", "2","--Py", "2", "--dt", "0.1","--T", "2", 
+    string n_input [n] = {" ","--Lx", "1.0", "--Ly", "1.0", "--Nx", "10", "--Ny", 
+                            "10", "--Px", "1","--Py", "1", "--dt", "0.001","--T", "0.005", 
                             "--Re", "100.0"};
     
     
@@ -22,8 +23,8 @@ int main(int argc, char *argv[]){
     // Configure the solver here...
     // ...
     
-    solver->Initialise(n, n_input);
-//    solver->Initialise(argc, argv);
+//    solver->Initialise(n, n_input);
+    solver->Initialise(argc, argv);
 
     // Run the solver
     solver->Integrate(argc, argv);
@@ -32,3 +33,8 @@ int main(int argc, char *argv[]){
 
 	return 0;
 }
+
+/* To run the code, use this command line syntax:
+ --Lx 1.0 --Ly 1.0 --Nx 10 --Ny 10 --Px 1 --Py 1 --dt 0.001 --T 0.005 --Re 100.0
+
+*/
