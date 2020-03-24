f85f68611d23aca73f3f2716cd9c9f7049d5a4cf
Easha Easha Tu Razia <eh2617@spitfire.ae.ic.ac.uk>
2020-03-24 20:39:57 +0000

    Functions in the LidDrivenCavity Class

--- LidDrivenCavity.h
+++ LidDrivenCavity.h
@@ -1,10 +1,9 @@
-
-#define LIDDRIVENCAVITY_H
+#define LIDDRIVENCAVITYJACOBI_H
 #pragma once
 /* preprocessor directive designed to cause the 
  * current source file to be included only once in a single compilation. */
 
-#include "Functions.h" //basic functions used throughout the code
+#include "PoissonSolver.h"
 
 class LidDrivenCavity
 {
@@ -14,14 +13,22 @@ public:
 
     void SetDomainSize(double xlen, double ylen);
     void SetGridSize(int nx, int ny);
+    void SetPartitionSize(int px, int py);
     void SetTimeStep(double deltat);
     void SetFinalTime(double finalt);
     void SetReynoldsNumber(double Re);
+    
 
-    void Initialise(int n, string *n_inputs);
-    void Integrate();
+    void Initialise(int &argc, char *argv[]);
+    void Integrate(int &argc, char *argv[]); //Solver for the Finite Difference method for the Lid Driven cavity
 
     // Add any other public functions
+    void BoundaryVorticity(); //Calculation of vorticity boundary conditions at time t 
+    void InteriorVorticity(); //Calculation of interior vorticity at time t
+    void NewInteriorVorticity(); // Calculation of interior vorticity at time t+dt
+    void PatchUp(); // Assembly of the processors
+    void BoundaryVorticity_serial(); //Calculation of vorticity boundary conditions at time t for the whole process
+    
 
 private:
     double* w = nullptr; // vorticity
@@ -34,105 +41,29 @@ private:
     double Lx;
     double Ly;
     double Re;
-    double U = 1.0; 
+    int    Px;
+    int    Py;
+    
+    const double U = 1.0;
+    double dx;
+    double dy;
+    int    mpirank;
+    int    np;
+    int    NBx; // size of blocks in a partition
+    int    NBy;
+    int    srw;
+    int    scl;
+    int    ctx, Tctx;
+    int    myrow, mycol;
+    int    Tmyrow, Tmycol;
+    int    Psize;
+    
+    double* W = nullptr; // vorticity
+    double* PSI = nullptr; // stream fuction
+    
+    // Communicator arrays
+    double *work_t = nullptr;
+    double *work_b = nullptr;
+    double *work_l = nullptr;
+    double *work_r = nullptr;
 };
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-LidDrivenCavity::LidDrivenCavity(){ // constructor
-}
-
-LidDrivenCavity::~LidDrivenCavity(){ //destructor
-    delete[] w;
-    delete[] psi;
-    
-}
-
-void LidDrivenCavity::SetDomainSize(double xlen, double ylen){
-    Lx = xlen;
-    Ly = ylen;
-}
-
-void LidDrivenCavity::SetGridSize(int nx, int ny){
-    Nx = nx;
-    Ny = ny;
-}
-
-void LidDrivenCavity::SetTimeStep(double del_t){
-    dt = del_t;
-}
-
-void LidDrivenCavity::SetFinalTime(double finalt){
-    T = finalt;
-}
-
-void LidDrivenCavity::SetReynoldsNumber(double re){
-    Re = re;
-}
-
-void LidDrivenCavity::Initialise(int n, string *n_inputs){
-    for(int i=1; i<n; i=i+2){
-        if (n_inputs[i] == "--Lx" && n_inputs[i+2] == "--Ly"){
-            LidDrivenCavity::SetDomainSize( stod(n_inputs[i+1]), stod(n_inputs[i+3]) );
-            //cout << "done 1" << endl;
-        }
-        else if(n_inputs[i] == "--Nx" && n_inputs[i+2] == "--Ny"){
-            LidDrivenCavity::SetGridSize( stoi(n_inputs[i+1]), stoi(n_inputs[i+3]) );
-            //cout << "done 2" << endl;
-        }
-        else if(n_inputs[i] == "--dt"){
-            LidDrivenCavity::SetTimeStep(stod(n_inputs[i+1]) );
-            //cout << "done 3" << endl;
-        }
-        else if(n_inputs[i] == "--T"){
-            LidDrivenCavity::SetFinalTime(stod(n_inputs[i+1]) );
-            //cout << "done 4" << endl;
-        }
-        else if(n_inputs[i] == "--Re"){
-            LidDrivenCavity::SetReynoldsNumber( stod(n_inputs[i+1]) );
-            //cout << "done 5" << endl;
-        }
-    }
-    
-}
-
-void LidDrivenCavity::Integrate()
-{
-     int srw = Nx -2;
-     int scl = Ny -2;
-     
-     double dx = Lx/(Nx-1);
-     double dy = Ly/(Ny-1);
-     
-     if (dt >= Re*dx*dy/4){
-         cout << "dt " << dt << ">=" << Re*dx*dy/4 << endl;
-         cout << "dt is too big! Please enter a smaller dt. ";
-         return;
-     }
-     
-     //const double U = 1.0;
-     
-     double *A = new double[(2*srw+1+srw)*srw*scl](); 
-     int *ipiv = new int[srw*scl*srw*scl];
-     double a = 2*(1/dx/dx + 1/dy/dy), b  = -1/dx/dx , c = -1/dy/dy;
-     BandedMatrixLapackLU(srw, scl, a, b, c, A, ipiv); // should be 2*srw+1 by srw^2 plus srw rows for padding
-     
-     //Initial Conditions
-     w = new double[Nx*Ny]();
-     psi = new double[Nx*Ny]();
-     
-     for (int nt=1; nt<T/dt; nt++){
-         BoundaryVorticity(Nx, Ny, dx, dy, U, psi, w);
-         
-         InteriorVorticity(Nx, Ny, dx, dy, psi, w);
-         
-         NewInteriorVorticity(Re, Nx, Ny, dx, dy, dt, psi, w);
-         
-         PoissonProblem(Nx, Ny, srw, scl, A, w, psi, ipiv);
-     }
-     //printMatrix( (2*srw+1+srw),srw*scl, A);
-     
-     cout << "w" << endl;
-    printMatrix( Nx, Ny, w);
-//    cout << "psi" << endl;
-//    printMatrix( Nx, Ny, psi);
-}
