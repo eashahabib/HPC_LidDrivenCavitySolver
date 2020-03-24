39b4bb055ba46ec56be23ce90afcc610c2ada2e8
Easha Easha Tu Razia <eh2617@spitfire.ae.ic.ac.uk>
2020-03-24 20:44:19 +0000

    General global functions including matrix printing and communicators and declarations

--- Functions.h
+++ Functions.h
@@ -3,15 +3,15 @@
 
 #pragma once
 #include <cmath>
-using namespace std;
 #include <iostream>
 #include <fstream>
 #include <stdlib.h> //for atof
 
+using namespace std;
+
 // This provides forward-declarations of the C++ iostream classes since
 // we do not need the full definition in the header file.
 #include <iosfwd>
-
 #include <cblas.h>
 
 
@@ -49,7 +49,7 @@ extern "C" {
     void Cblacs_barrier(int, const char*);
     void Cblacs_gridinfo(int ctx, int *nrow, int *ncol, int *myrow, int *mycol); // Get info about the grid to verify it is set up
     
-    int numroc_(int*, int*, int*, int*, int*);
+    int numroc_(int *N, int *Nb, int *iproc, int *isrcproc, int *nprocs);
     
     /* Scalapack declarations */
     void F77NAME(pdgbsv)(const int &N, const int &BWL, const int &BWU, const int &NRHS, 
@@ -73,8 +73,8 @@ extern "C" {
 
 void printMatrix(int row, int col, double* A);
 void printMatrix(int row, int col, int* A);
-void Print2File(int argc, char *argv[], int Nx, int Ny, double *w, double *psi);
-void Update(int mpirank, int Tmycol, int Tmyrow, int NBx, int NBy, int Px, int Py, double *w);
+void Print2File(int argc, char *argv[], int Nx, int Ny, double *w, double *psi, string ext);
+void Update(int mpirank, int Tmycol, int Tmyrow, int NBx, int NBy, int Px, int Py, double *w, double *top, double *bot, double *left, double *right);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -104,19 +104,34 @@ void printMatrix(int row, int col, int* A) {
 cout << "-----------------------------" << endl;
 }
 
-void Print2File(int argc, char *argv[], int Nx, int Ny, double *w, double *psi){
-    ofstream vfile("Data_LidDrivenCavity.txt" ,ios::trunc | ios::in);
+void Print2File(int argc, char *argv[], int Nx, int Ny, double *w, double *psi, string ext = "txt"){
+    string filename;
+    for(int i=1; i<argc; i++){
+            filename += argv[i];
+        }
+    filename += "." + ext;
+    
+    string delimiter;
+    if(ext == "csv"){
+        delimiter = ",";
+    }else{
+        delimiter = " ";
+    }
+    
+    
+    ofstream vfile(filename ,ios::trunc | ios::in);
     if(vfile.good()){
         for(int i=1; i<argc; i++){
             vfile << argv[i] << " ";
         }
         vfile << endl;
+        vfile << "Vorticity" << delimiter << "StreamFunction" << endl;
         
         for(int i=0;i<Nx;i++){
             for(int j=0; j<Ny; j++){
-                vfile.width(20);
-                vfile.precision(15);
-                vfile << w[i*Ny+j] <<" "<< psi[i*Ny+j] << endl; // outputting into the file
+                vfile.width(25);
+                vfile.precision(20);
+                vfile << w[i*Ny+j] << delimiter << psi[i*Ny+j] << endl; // outputting into the file
             }
             
         }
@@ -127,64 +142,11 @@ void Print2File(int argc, char *argv[], int Nx, int Ny, double *w, double *psi){
     }
 }
 
-void Update(int mpirank, int Tmycol, int Tmyrow, int NBx, int NBy, int Px, int Py, double *w){
-    double *top = new double[NBx-2];
-    double *bot = new double[NBx-2];
-    double *left = new double[NBy-2];
-    double *right = new double[NBy-2];
-    
-    if(Py != 1){
-        if(Tmycol != Py-1 && Tmycol != 0){ // Middle column
-            for(int i=1; i<NBx-1; i++){
-                top[i-1] = w[(NBy-2)*NBx+i];
-                bot[i-1] = w[1*NBx+i];
-            }
-            
-            MPI_Send(top, NBx-2, MPI_DOUBLE, mpirank+Px, 1, MPI_COMM_WORLD);
-            MPI_Send(bot, NBx-2, MPI_DOUBLE, mpirank-Px, 2, MPI_COMM_WORLD);
-            
-        }else if(Tmycol == Py-1){// Left column (Top)
-        
-            for(int i=1; i<NBx-1; i++){
-                bot[i-1] = w[1*NBx+i];
-            }
-            
-            MPI_Send(bot, NBx-2, MPI_DOUBLE, mpirank-Px, 2, MPI_COMM_WORLD);
-            
-        }else if(Tmycol == 0){ // Right Column (Bottom)
-            
-            for(int i=1; i<NBx-1; i++){
-                top[i-1] = w[(NBy-2)*NBx+i];
-            }
-            
-            MPI_Send(top, NBx-2, MPI_DOUBLE, mpirank+Px, 1, MPI_COMM_WORLD);
-        }
-        
-        if(Tmycol != Py-1 && Tmycol != 0){ // Middle column
-        
-            MPI_Recv(top, NBx-2, MPI_DOUBLE, mpirank+Px, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Recv(bot, NBx-2, MPI_DOUBLE, mpirank-Px, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            
-            for(int i=1; i<NBx-1; i++){
-                w[(NBy-1)*NBx+i] = top[i-1];
-                w[0*NBx+i] = bot[i-1];
-            }
-        }else if(Tmycol == Py-1){// Left column (Top)
-            
-            MPI_Recv(bot, NBx-2, MPI_DOUBLE, mpirank-Px, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            
-            for(int i=1; i<NBx-1; i++){
-                w[0*NBx+i] = bot[i-1];
-            }
-        }else if(Tmycol == 0){ // Right Column (Bottom)
-            
-            MPI_Recv(top, NBx-2, MPI_DOUBLE, mpirank+Px, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            
-            for(int i=1; i<NBx-1; i++){
-                w[(NBy-1)*NBx+i] = top[i-1];
-            }
-        }
-    }
+void Update(int mpirank, int Tmycol, int Tmyrow, int NBx, int NBy, int Px, int Py, double *w, double *top, double *bot, double *left, double *right){
+//    top = new double[NBx-2];
+//    bot = new double[NBx-2];
+//    left = new double[NBy-2];
+//    right = new double[NBy-2];
     
     if(Px != 1){
         if(Tmyrow != Px-1 && Tmyrow != 0){ // Middle row
@@ -200,7 +162,7 @@ void Update(int mpirank, int Tmycol, int Tmyrow, int NBx, int NBy, int Px, int P
         }else if(Tmyrow == Px-1){// Bottom row (Right)
             
             for(int j=1; j<NBy-1; j++){
-                right[j-1] = w[j*NBx+(NBx-2)];
+                right[j-1] = w[j*NBx+1];
             }
             
             MPI_Send(right, NBy-2, MPI_DOUBLE, mpirank-1, 0, MPI_COMM_WORLD);
@@ -208,11 +170,42 @@ void Update(int mpirank, int Tmycol, int Tmyrow, int NBx, int NBy, int Px, int P
         }else if(Tmyrow == 0){
             
             for(int j=1; j<NBy-1; j++){
-                left[j-1] = w[j*NBx+1];
+                left[j-1] = w[j*NBx+(NBx-2)];
             }
             
             MPI_Send(left, NBy-2, MPI_DOUBLE, mpirank+1, 0, MPI_COMM_WORLD);
         }
+    }
+    
+    if(Py != 1){
+        if(Tmycol != Py-1 && Tmycol != 0){ // Middle column
+            for(int i=1; i<NBx-1; i++){
+                top[i-1] = w[(NBy-2)*NBx+i];
+                bot[i-1] = w[1*NBx+i];
+            }
+            
+            MPI_Send(top, NBx-2, MPI_DOUBLE, mpirank+Px, 0, MPI_COMM_WORLD);
+            MPI_Send(bot, NBx-2, MPI_DOUBLE, mpirank-Px, 0, MPI_COMM_WORLD);
+            
+        }else if(Tmycol == Py-1){// Right column (Top)
+        
+            for(int i=1; i<NBx-1; i++){
+                bot[i-1] = w[1*NBx+i];
+            }
+            
+            MPI_Send(bot, NBx-2, MPI_DOUBLE, mpirank-Px, 0, MPI_COMM_WORLD);
+            
+        }else if(Tmycol == 0){ // Left Column (Bottom)
+            
+            for(int i=1; i<NBx-1; i++){
+                top[i-1] = w[(NBy-2)*NBx+i];
+            }
+            
+            MPI_Send(top, NBx-2, MPI_DOUBLE, mpirank+Px, 0, MPI_COMM_WORLD);
+        }
+    }
+    
+    if(Px != 1){
         
         if(Tmyrow != Px-1 && Tmyrow != 0){ // Middle row
         
@@ -220,8 +213,8 @@ void Update(int mpirank, int Tmycol, int Tmyrow, int NBx, int NBy, int Px, int P
             MPI_Recv(right, NBy-2, MPI_DOUBLE, mpirank+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             
             for(int j=1; j<NBy-1; j++){
-                w[j*NBx+0] = right[j-1];
-                w[j*NBx+(NBx-1)] = left[j-1];
+                w[j*NBx+0] = left[j-1];
+                w[j*NBx+(NBx-1)] = right[j-1];
             }
         }else if(Tmyrow == Px-1){// Bottom row (Right)
             
@@ -240,13 +233,34 @@ void Update(int mpirank, int Tmycol, int Tmyrow, int NBx, int NBy, int Px, int P
         }
     }
     
-//    if(Py != 1){
-//        
-//    }
+    if(Py != 1){
+        if(Tmycol != Py-1 && Tmycol != 0){ // Middle column
         
-//    if(Px != 1){
-//        
-//    }
+            MPI_Recv(top, NBx-2, MPI_DOUBLE, mpirank+Px, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(bot, NBx-2, MPI_DOUBLE, mpirank-Px, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            
+            for(int i=1; i<NBx-1; i++){
+                w[(NBy-1)*NBx+i] = top[i-1];
+                w[0*NBx+i] = bot[i-1];
+            }
+        }else if(Tmycol == Py-1){// Righht most column (Top)
+            
+            MPI_Recv(bot, NBx-2, MPI_DOUBLE, mpirank-Px, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            
+            for(int i=1; i<NBx-1; i++){
+                w[0*NBx+i] = bot[i-1];
+            }
+        }else if(Tmycol == 0){ // Left most Column (Bottom)
+            
+            MPI_Recv(top, NBx-2, MPI_DOUBLE, mpirank+Px, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            
+            for(int i=1; i<NBx-1; i++){
+                w[(NBy-1)*NBx+i] = top[i-1];
+            }
+        }
+    }
+        
+//    delete [] top, bot, right, left;
 }
 
 
